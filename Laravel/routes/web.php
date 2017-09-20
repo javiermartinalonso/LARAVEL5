c19d79e5fe00ff1b223984fecc266fdcb3aec3dd
Javier Martín Alonso <xavimartin@hotmail.com>
2017-09-20 09:51:39 +0200

    rutas

---
+++ Laravel/routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return 'hola mundo';
+});
+
+
+Route::get('testing/cualquiera/nuevo', function () {
+    return 'aprendiendo rutas';
+});
