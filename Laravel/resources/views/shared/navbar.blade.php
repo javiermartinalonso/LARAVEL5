71617c37b70efb6478070536bb0ebd6dc22e8e4a
Javier Martín Alonso <xavimartin@hotmail.com>
2017-09-20 13:29:36 +0200

    motor de plantillas blade

---
+++ Laravel/resources/views/shared/navbar.blade.php
@@ -0,0 +1,54 @@
+<nav class="navbar navbar-default">
+	<div class="container-fluid">
+		<!-- Brand and toggle get grouped for better mobile display -->
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed" data-toggle="c\
+			ollapse" data-target="#bs-example-navbar-collapse-1">
+			<span class="sr-only">Toggle navigation</span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+		</button>
+		<a class="navbar-brand" href="#">Brand</a>
+	</div>
+	<!-- Collect the nav links, forms, and other content for toggling -->
+	<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+		<ul class="nav navbar-nav">
+		<li class="active"><a href="#">Enlace <span class="sr-only">(current)</span></a></li>
+				<li><a href="#">Enlace</a></li>
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
+					<ul class="dropdown-menu" role="menu">
+						<li><a href="#">Acción</a></li>
+						<li><a href="#">Otra Acción</a></li>
+						<li><a href="#">Algo más aquí</a></li>
+						<li class="divider"></li>
+						<li><a href="#">Enlace separado</a></li>
+						<li class="divider"></li>
+						<li><a href="#">Otro enlace separado</a></li>
+					</ul>
+				</li>
+			</ul>
+			<form class="navbar-form navbar-left" role="search">
+				<div class="form-group">
+					<input type="text" class="form-control" placeholder="Search">
+				</div>
+				<button type="submit" class="btn btn-default">Enviar</button>
+			</form>
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="#">Enlace</a></li>
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown" r\
+					ole="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
+					<ul class="dropdown-menu" role="menu">
+						<li><a href="#">Acción</a></li>
+						<li><a href="#">Otra Acción</a></li>
+						<li><a href="#">Algo más aquí</a></li>
+						<li class="divider"></li>
+						<li><a href="#">Enlace separado</a></li>
+					</ul>
+				</li>
+			</ul>
+		</div><!-- /.navbar-collapse -->
+	</div><!-- /.container-fluid -->
+</nav>
